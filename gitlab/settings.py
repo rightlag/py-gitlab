023dc37cb3ff9b969a52030db6c5f3c3ae920219
rightlag <rightlag@gmail.com>
2015-11-27 17:59:15 -0800

    Catch OSError is settings module if ~/.gitlab config file cannot be found

--- gitlab/settings.py
+++ gitlab/settings.py
@@ -3,6 +3,9 @@ import os
 
 
 class ConfigParser(object):
+    def __init__(self):
+        self._config = {}
+
     def read(self, path):
         path = os.path.expanduser(path)
         if not os.path.exists(path):
@@ -19,8 +22,9 @@ class ConfigParser(object):
         except ValueError, e:
             raise e
 
-
-config = ConfigParser()
-config.read('~/.gitlab')
-
-PRIVATE_TOKEN = config.get('private_token')
+try:
+    config = ConfigParser()
+    config.read('~/.gitlab')
+    PRIVATE_TOKEN = config.get('private_token')
+except OSError:
+    PRIVATE_TOKEN = None
